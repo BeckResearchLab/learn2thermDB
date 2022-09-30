8cdff79d6eca679a597a74aae1ae7dd644237ba4
evankomp@uw.edu <evankomp@uw.edu>
2022-09-30 11:04:11 -0700

    positional files

--- pipeline/s0.0_get_ncbi_refseq.sh
+++ pipeline/s0.0_get_ncbi_refseq.sh
@@ -5,12 +5,18 @@ USER=anonymous
 PASSWORD=$ENV_EMAIL
 
 # create subdirectory if necessary
-mkdir -p ../data/refseq
-mkdir -p ../data/refseq/archaea
-mkdir -p ../data/refseq/bacteria
+mkdir -p ./data/refseq
+mkdir -p ./data/refseq/archaea
+mkdir -p ./data/refseq/bacteria
 
 # download archaea data
-cd ../data/refseq/archaea
-lftp -c "open -u $USER,$PASSWORD $HOST; cd /genomes/refseq/archaea; mget -c -O './' /genomes/refseq/archaea/*/latest_assembly_versions/*/*.gbff.gz"
+cd ./data/refseq/archaea
+lftp -c "open -u $USER,$PASSWORD $HOST; cd /genomes/refseq/archaea; mget -c -O './' /genomes/refseq/archaea/*/latest_assembly_versions/*/*genomic.gbff.gz"
+# unzip all
+for FILE in *; do gzip -d $FILE; done
+
+# download bacteria data
+cd ../bacteria
+lftp -c "open -u $USER,$PASSWORD $HOST; cd /genomes/refseq/bacteria; mget -c -O './' /genomes/refseq/bacteria/*/latest_assembly_versions/*/*genomic.gbff.gz"
 # unzip all
 for FILE in *; do gzip -d $FILE; done
