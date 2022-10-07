c91a5f4964680dde1ecf44beafd7a76214ec7719
evankomp@uw.edu <evankomp@uw.edu>
2022-10-07 09:00:41 -0700

    extracted all proteins

--- pipeline/s1.1_get_protein_sequences.py
+++ pipeline/s1.1_get_protein_sequences.py
@@ -2,6 +2,7 @@
 import logging
 import os
 import time
+import fcntl
 
 from joblib import delayed, Parallel
 import pandas as pd
@@ -20,7 +21,11 @@ else:
 LOGNAME = __file__
 LOGFILE = f'./logs/{os.path.basename(__file__)}.log'
 
-def extract_proteins_from_one(filepath: str):
+# where we will deposit_data
+OUTPUT_FILE_PROTEINS = './data/taxa/proteins.csv'
+OUTPUT_FILE_16srRNA = './data/taxa/16s_rRNA.csv'
+
+def extract_proteins_from_one(taxa_index: int, filepath: str):
     """Open a zipped GBFF file and extract all protein sequences.
     
     Parameters
@@ -59,10 +64,23 @@ def extract_proteins_from_one(filepath: str):
                     protein_sequences['desc'].append(None)
     if seq_16srRNA is None:
         logger.info(f"Could not find 16s rRNA in {filepath}")
-    logger.info(f"Found {len(protein_sequences)} proteins")
+    logger.info(f"Found {len(protein_sequences['sequence'])} proteins")
     protein_sequences['seq_len'] = [len(s) for s in protein_sequences['sequence']]
-    protein_sequences = pd.DataFrame(protein_sequences)
-    return protein_sequences, seq_16srRNA
+    
+    # save to file
+    with open(OUTPUT_FILE_PROTEINS, "a") as g:
+        fcntl.flock(g, fcntl.LOCK_EX)
+        for i in range(len(protein_sequences['seq_len'])):
+            g.write(f"{taxa_index},{protein_sequences['sequence'][i]},{protein_sequences['desc'][i]},{protein_sequences['seq_len'][i]}\n")
+        fcntl.flock(g, fcntl.LOCK_UN)
+    with open(OUTPUT_FILE_16srRNA, "a") as g:
+        fcntl.flock(g, fcntl.LOCK_EX)
+        g.write(f"{taxa_index},{seq_16srRNA}\n")
+        fcntl.flock(g, fcntl.LOCK_UN)
+    
+    num_proteins = len(protein_sequences['sequence'])
+    has_16srRNA = seq_16srRNA is not None
+    return num_proteins, has_16srRNA
 
 if __name__ == '__main__':
     # connect to log file
@@ -80,28 +98,24 @@ if __name__ == '__main__':
         taxa = taxa.iloc[:params['n_sample']]
         logger.info(f"Using only the first {params['n_sample']} files.")
 
+    # start the files, we have to write intermittedly because of memory issues
+    with open(OUTPUT_FILE_PROTEINS, "w") as g:
+        g.write("taxa_index,protein_seq,protein_desc,protein_len\n")
+    with open(OUTPUT_FILE_16srRNA, "w") as g:
+        g.write("taxa_index,seq_16srRNA\n")
+
     # get the info in parallel
-    outputs = Parallel(n_jobs=params['n_jobs'])(delayed(extract_proteins_from_one)(file) for file in taxa.values)
+    outputs = Parallel(n_jobs=params['n_jobs'])(delayed(extract_proteins_from_one)(taxa_index, file) for taxa_index, file in taxa.items())
 
     # process into two files, one for proteins, one for 16s rRNA
-    proteins, taxa_16srRNA = list(zip(*outputs))
+    proteins_counts, has_16srRNA = list(zip(*outputs))
     # proteins is a list of dataframe, taxa_16srRNA is a list of string or none if not found
-    assert len(proteins) == len(taxa)
-    assert len(taxa_16srRNA) == len(taxa)
-
-    # assign the taxa index to every protein from that organism
-    for i, protein_df in enumerate(proteins):
-        protein_df['taxa_index'] = taxa.index[i]
-    proteins = pd.concat(proteins, ignore_index=True)
-    proteins.to_csv('./data/taxa/proteins.csv')
-
-    # now save 16s
-    taxa_16srRNA = pd.Series(index=taxa.index, data=taxa_16srRNA)
-    taxa_16srRNA.to_csv('./data/taxa/16s_rRNA.csv')
+    assert len(proteins_counts) == len(taxa)
+    assert len(has_16srRNA) == len(taxa)
 
     # save metrics
     metrics = {}
-    metrics['n_total_sequences'] = int(len(proteins))
-    metrics['n_taxa_with_16srRNA'] = int((~taxa_16srRNA.isna()).sum())
+    metrics['n_total_sequences'] = int(sum(proteins_counts))
+    metrics['n_taxa_with_16srRNA'] = int(sum(has_16srRNA))
     with open('./data/metrics/s1.1_metrics.yaml', "w") as stream:
         yaml_dump(metrics, stream)
