640604381a9610fd6e5d4290efd019e98b84ba38
evankomp@uw.edu <evankomp@uw.edu>
2023-02-16 09:26:35 -0800

    need to run DB creation on hyak

--- learn2therm/database.py
+++ learn2therm/database.py
@@ -85,11 +85,6 @@ class L2TDatabase:
         cls._create_protein_pairs_table(conn, files_path)
         t2 = time.time()
         logger.info(f"Took {(t2-t1)/60}m to create protein pair table")
-
-        t1 = time.time()
-        cls._create_indexes(conn, files_path)
-        t2 = time.time()
-        logger.info(f"Took {(t2-t1)/60}m to create indexes")
     
         conn.close()        
         return cls(db_path, read_only=read_only)
@@ -278,7 +273,6 @@ class L2TDatabase:
         conn.execute("CREATE INDEX prot_pair_both ON protein_pairs (meso_index, thermo_index)")
         conn.execute("CREATE INDEX prot_pair_to_meso_prot ON protein_pairs (meso_protein_int_index)")
         conn.execute("CREATE INDEX prot_pair_to_thermo_prot ON protein_pairs (thermo_protein_int_index)")
-        conn.execute("CREATE INDEX prot_pair_both_prot ON protein_pairs (meso_protein_int_index, thermo_protein_int_index)")
         conn.execute("CREATE INDEX prot_pair_to_taxa_pair ON protein_pairs (taxa_pair_index)")
         conn.execute("CHECKPOINT")
 
