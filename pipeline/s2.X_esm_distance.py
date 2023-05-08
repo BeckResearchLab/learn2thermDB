0260b4c7e74df984ee9370665326648a89b60ceb
Evan Komp <evankomp@uw.edu>
2023-05-08 13:25:53 -0700

    stub scripts for two validation steps

---
+++ pipeline/s2.X_esm_distance.py
@@ -0,0 +1,4 @@
+"""Compare ESM distance of observed protein pairs to random pairs.
+
+Actual pairs should be statistically significantly different than random pairs.
+"""
---
+++ pipeline/s2.X_known_modes.py
@@ -0,0 +1,7 @@
+"""Compare observed protein pairs to concensus modes of thermal stability.
+
+Eg. less amide AAs, percent of charged residues, etc.
+
+Make bins of delta T pairs, look at T (other better for percent?) test between percentages as delta T changes.
+Look for statistically significant differences between modes at each bin.
+"""
