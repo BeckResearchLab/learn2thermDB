a7e0ff970922e080e1e5305a4977be3503787a62
Evan Komp <evankomp@uw.edu>
2023-05-18 10:24:03 -0700

    ran fireprotdb agains out proteins with diamond to plot Tm vs OGT. 32 points indicate a .25 corelation with 85% significance. Also ran ESM. Hait pairs are close to random pairs, while our pairs have statistically significant smaller ESM distance

--- pipeline/s2.X_compare_to_Tm.py
+++ pipeline/s2.X_compare_to_Tm.py
@@ -3,6 +3,7 @@ and see if they correspond to higher OGT on average.
 """
 import os
 import shutil
+import time
 import io
 from yaml import safe_load as yaml_load
 from yaml import dump as yaml_dump
@@ -25,6 +26,7 @@ import logging
 
 import learn2therm.database
 import learn2therm.utils
+import learn2therm.blast
 
 if 'LOGLEVEL' in os.environ:
     LOGLEVEL = os.environ['LOGLEVEL']
@@ -46,7 +48,7 @@ if __name__ == "__main__":
     response = requests.post(FIREPROTDB_URL, json=payload)
     content = response.content.decode('utf-8')
 
-    tm_df = pd.read_csv(io.StringIO(content))[['uniprot_id', 'tm', 'dTm']]
+    tm_df = pd.read_csv(io.StringIO(content))[['uniprot_id', 'tm', 'dTm', 'sequence']]
     logger.info(f"{len(tm_df)} raw data points from FireProtDB")
 
     # clean up the data  abit
@@ -56,33 +58,73 @@ if __name__ == "__main__":
     logger.info(f"{len(tm_df)} data points after dropping duplicate uniprot ids")
     tm_df['wTm'] = tm_df['tm'] - tm_df['dTm']
     logger.info(f"Tm data: {tm_df['wTm'].describe()}")
+    tm_df.rename(columns={'uniprot_id': 'pid'}, inplace=True)
 
     # spin up database
     tmpfile = tempfile.NamedTemporaryFile(dir='./tmp', mode='w+b', delete = True)
     tmpfile.close()
-    logger.info("Creating database referencing 3rd party data at %s", tmpfile.name)
+    logger.info("Creating database  at %s", tmpfile.name)
     con = ddb.connect(database=tmpfile.name, read_only=False)
     learn2therm.database.L2TDatabase._create_taxa_table(con, './data/')
     learn2therm.database.L2TDatabase._create_proteins_table(con, './data/')
     logger.info("Created learn2therm database")
-    # add meltome data
+
+    # need to search out dataset of proteins for matches
+    # since uniprot ids are not necessarily non redundant for >99% identity
+    # we need to search by sequence
+    our_proteins = con.execute("SELECT pid, protein_seq AS sequence FROM proteins ORDER BY RANDOM()").df()
+    logger.info(f"Got {len(our_proteins)} proteins from our dataset")
+    
+    handler = learn2therm.blast.DiamondAlignmentHandler(
+        seqs_A = tm_df,
+        seqs_B = our_proteins,
+        metrics = ['scaled_local_query_percent_id'],
+        alignment_params = {
+            'num_threads': 6,
+            'sensitivity': 'fast',
+            'iterate': False,              
+            'global_ranking': None,           
+            'gapopen': 11,
+            'gapextend': 2,
+            'matrix': 'BLOSUM62',
+            'evalue': .0000001,
+            'hsp_cov': 99,  
+        }
+    )
+    time0 = time.time()
+    logger.info("Running blast")
+    results, metadata = handler.run()
+    logger.info(f"Blast took {(time.time() - time0)/60} minutes")
+    logger.info(f"Got {len(results)} results from blast")
+
+    # aggregate for the best result for each query
+    results = results.sort_values('scaled_local_query_percent_id', ascending=False).drop_duplicates(subset=['query_id'])
+    logger.info(f"Got {len(results)} results after taking best match for each query")
+    logger.info(f"Results description: {results['scaled_local_query_percent_id'].describe()}")
+
+    # merge with tm data
+    tm_df = tm_df.merge(results, left_on='pid', right_on='query_id', how='inner')
+    # drop those without 99% identity
+    tm_df = tm_df[tm_df['scaled_local_query_percent_id'] >= .99][['subject_id', 'wTm']]
+    logger.info(f"Protein map: {tm_df[['query_id', 'subject_id']]}")
+    logger.info(f"Got {len(tm_df)} results after dropping those without 99% identity")
+    # clear some memory
+    del our_proteins
+
+    # add wTm data
     con.execute("CREATE OR REPLACE TABLE tm AS SELECT * FROM tm_df")
     logger.info("Added tm data")
     con.execute("CREATE UNIQUE INDEX taxa_primary ON taxa (taxid)")
     con.execute("CREATE UNIQUE INDEX prot_primary ON proteins (pid)")
     con.execute("CREATE INDEX prot_taxa_foreign ON proteins (taxid)")
-    con.execute("CREATE INDEX tm_primary ON tm (uniprot_id)")
+    con.execute("CREATE INDEX tm_primary ON tm (subject_id)")
     logger.info("Created indexes")
 
     # run a join on data to get ogt vs meting temp
-    tm_data_not_present = con.execute("""
-        SELECT tm.uniprot_id FROM tm LEFT JOIN proteins ON tm.uniprot_id = proteins.pid WHERE proteins.pid IS NULL
-    """).df()
-    logger.info(f"3rd part uniprot proteins not in our data: {tm_data_not_present}")
     data = con.execute("""
         SELECT tm.wTm, taxa.temperature FROM taxa
         INNER JOIN proteins ON proteins.taxid = taxa.taxid
-        INNER JOIN tm ON tm.uniprot_id = proteins.pid
+        INNER JOIN tm ON tm.subject_id = proteins.pid
     """).df()
     con.close()
     os.remove(tmpfile.name)
--- pipeline/s2.X_esm_distance.py
+++ pipeline/s2.X_esm_distance.py
@@ -3,12 +3,13 @@
 Actual pairs should be statistically significantly different than random pairs.
 """
 import os
+import json
 from yaml import safe_load as yaml_load
+from yaml import dump as yaml_dump
 import pathlib
 import tempfile
-import torch
-from esm import Alphabet, FastaBatchedDataset, pretrained, MSATransformer
-import duckdb as ddb
+import requests
+import time
 
 import learn2therm.blast
 import learn2therm.utils
@@ -17,6 +18,9 @@ import pandas as pd
 import scipy.stats
 import matplotlib.pyplot as plt
 import seaborn as sns
+import torch
+from esm import Alphabet, FastaBatchedDataset, pretrained, MSATransformer
+import duckdb as ddb
 sns.set_style('whitegrid')
 sns.set_context('paper')
 
@@ -36,6 +40,9 @@ LOGNAME = __file__
 LOGFILE = f'./logs/{os.path.basename(__file__)}.log'
 # get the logger in subprocesses
 logger = learn2therm.utils.start_logger_if_necessary(LOGNAME, LOGFILE, LOGLEVEL, filemode='w')
+
+LOAD_FILE = True
+HAIT_URL = 'https://onlinelibrary.wiley.com/action/downloadSupplement?doi=10.1002%2Fprot.25866&file=prot25866-sup-0001-DataS1.xlsx'
 
 def process_fasta_file(
     model_location: str,
@@ -132,10 +139,17 @@ def run_esm_on_df(dataframe, model='esm2_t36_3B_UR50D'):
     """Create fasta files and run ESM"""
 
     # need iter to make blast files
+    # if there are duplicate sequences, only keep one, they will be merged later
     records = []
+    ids_ = []
     for i, row in dataframe.iterrows():
-        record = SeqRecord(Seq(row['protein_seq']), id=row['pid'], description='')
-        records.append(record)
+        if row['pid'] in ids_:
+            continue
+        else:
+            record = SeqRecord(Seq(row['protein_seq']), id=row['pid'], description='')
+            records.append(record)
+            ids_.append(row['pid'])
+
     
     # write fasta file
     fasta_file = tempfile.NamedTemporaryFile(mode='w', dir='./tmp/', delete=False)
@@ -171,108 +185,177 @@ if __name__ == '__main__':
     temps = np.linspace(params['label_taxa']['ogt_threshold'], 75.0, 3)
     logger.info(f"Got temperatures to use for random thermo proteins: {temps}")
 
-    # connect to database and get protein pairs, as well as random pairs
-    con = ddb.connect('./tmp/database', read_only=True)
-    random_thermo_proteins_dict = {}
-    for T in temps:
-        random_thermo_proteins_dict[T] = con.execute(f"""
+    if LOAD_FILE:
+        random_distances = torch.load('./tmp/random_distances.pt')
+        actual_distances = torch.load('./tmp/actual_distances.pt')
+        hait_distances = torch.load('./tmp/hait_distances.pt')
+        logger.info('Loaded precomputed distances')
+    else:
+        # get hait data
+        hait_ht_pairs = pd.read_excel(
+            './tmp/prot25866-sup-0001-datas1.xlsx',
+            sheet_name='PAIRS',
+            usecols="A:B",
+            skiprows=8).rename(columns={'HT':'T'}).dropna()
+        hait_t_pairs = pd.read_excel(
+            './tmp/prot25866-sup-0001-datas1.xlsx',
+            sheet_name='PAIRS',
+            usecols="D:E",
+            skiprows=8).dropna().rename(columns={'T':'T', 'M.1':'M'})
+        hait_pairs = pd.concat([hait_ht_pairs, hait_t_pairs], join='outer', axis=0, ignore_index=True)
+        logger.info(f"Got HAIT data with {len(hait_pairs)} pairs")
+        # query for AA sequences from UniProt
+        base_q = 'https://rest.uniprot.org/uniprotkb/search?query='
+        pdbs = list(hait_pairs['M'].values.reshape(-1))
+        pdbs.extend(list(hait_pairs['T'].values.reshape(-1)))
+        pdbs = list(set(pdbs))
+        # programatic access in chunks
+        n = 20
+        pdb_chunks = [pdbs[i * n:(i + 1) * n] for i in range((len(pdbs) + n - 1) // n )]
+        pdb_to_seq = {}
+        for chunk in pdb_chunks:
+            q = [f'(xref:pdb-{p})OR' for p in chunk]
+            q = ''.join(q)[:-2]
+            q = base_q+q
+            r = requests.get(q)
+            r.raise_for_status()
+            logger.info(f"Got response from UniProt for a chunk of HAIT data")
+            results = r.json()
+            results = results['results']
+            # map pdb id to seq
+            for result in results:
+                seq = result['sequence']['value']
+                xrefs = result['uniProtKBCrossReferences']
+                for xref in xrefs:
+                    if xref['database'] == 'PDB':
+                        pdb_to_seq[xref['id']] = seq
+            time.sleep(3)
+        # create dataframe with seqs
+        hait_pairs['M_seq'] = hait_pairs['M'].map(pdb_to_seq)
+        hait_pairs['T_seq'] = hait_pairs['T'].map(pdb_to_seq)
+        hait_pairs.to_csv('./tmp/hait_pairs.csv')
+        hait_pairs.dropna(inplace=True)
+        logger.info(f"Got HAIT data with {len(hait_pairs)} pairs after dropping NA")
+        # get ESM distances
+        df_ = hait_pairs[['M_seq', 'M']].rename(columns={'M_seq':'protein_seq', 'M':'pid'})
+        hait_meso_esm_result = run_esm_on_df(df_)
+        df_ = hait_pairs[['T_seq', 'T']].rename(columns={'T_seq':'protein_seq', 'T':'pid'})
+        hait_thermo_esm_result = run_esm_on_df(df_)
+        # get distances
+        meso_tensor = torch.vstack(tuple(hait_meso_esm_result['tensor'].values))
+        thermo_tensor = torch.vstack(tuple(hait_thermo_esm_result['tensor'].values))
+        assert len(meso_tensor) == len(thermo_tensor)
+        distances = (meso_tensor - thermo_tensor).pow(2).sum(axis=1).sqrt()
+        logger.info(f"Mean, std of ESM distance for Hait pairs: {distances.mean()}, {distances.std()}")
+        torch.save(distances, './tmp/hait_distances.pt')
+
+        # connect to database and get protein pairs, as well as random pairs
+        con = ddb.connect('./tmp/database', read_only=True)
+        random_thermo_proteins_dict = {}
+        for T in temps:
+            random_thermo_proteins_dict[T] = con.execute(f"""
+                SELECT pid, protein_seq, temperature FROM proteins
+                INNER JOIN taxa ON proteins.taxid = taxa.taxid
+                WHERE temperature>{T}
+                ORDER BY RANDOM()
+                LIMIT 100
+            """).df()
+        random_meso_proteins = con.execute(f"""
             SELECT pid, protein_seq, temperature FROM proteins
             INNER JOIN taxa ON proteins.taxid = taxa.taxid
-            WHERE temperature>{T}
+            WHERE temperature<{params['label_taxa']['ogt_threshold']}
             ORDER BY RANDOM()
             LIMIT 100
         """).df()
-    random_meso_proteins = con.execute(f"""
-        SELECT pid, protein_seq, temperature FROM proteins
-        INNER JOIN taxa ON proteins.taxid = taxa.taxid
-        WHERE temperature<{params['label_taxa']['ogt_threshold']}
-        ORDER BY RANDOM()
-        LIMIT 100
-    """).df()
-    logger.info(f"Got  random mesophilic proteins and sets of random themo ones based on T")
-    # now get actual pairs
-    actual_protein_pairs = {}
-    for T in temps:
-        ids = con.execute(f"""
-            SELECT meso_pid, thermo_pid FROM pairs
-            INNER JOIN taxa AS thermo_taxa ON pairs.thermo_taxid = thermo_taxa.taxid
-            WHERE thermo_taxa.temperature>={T}
-        """).df()
-        meso_sequences = con.execute(f"""
-            SELECT pid, protein_seq, temperature FROM proteins
-            INNER JOIN taxa ON proteins.taxid = taxa.taxid
-            WHERE pid IN {tuple(ids['meso_pid'])}
-        """).df()
-        thermo_sequences = con.execute(f"""
-            SELECT pid, protein_seq, temperature FROM proteins
-            INNER JOIN taxa ON proteins.taxid = taxa.taxid
-            WHERE pid IN {tuple(ids['thermo_pid'])}
-        """).df()
-        actual_protein_pairs[T] = (meso_sequences, thermo_sequences)
-    con.close()
+        logger.info(f"Got  random mesophilic proteins and sets of random themo ones based on T")
+        # now get actual pairs
+        actual_protein_pairs = {}
+        for T in temps:
+            data = con.execute(f"""
+                SELECT 
+                    meso_proteins.pid AS meso_pid,
+                    thermo_proteins.pid AS thermo_pid, 
+                    meso_proteins.protein_seq AS meso_seq,
+                    thermo_proteins.protein_seq AS thermo_seq,
+                    meso_taxa.temperature AS meso_temp,
+                    thermo_taxa.temperature AS thermo_temp
+                FROM pairs
+                INNER JOIN taxa AS thermo_taxa ON pairs.thermo_taxid = thermo_taxa.taxid
+                INNER JOIN taxa AS meso_taxa ON pairs.meso_taxid = meso_taxa.taxid
+                INNER JOIN proteins AS thermo_proteins ON pairs.thermo_pid = thermo_proteins.pid
+                INNER JOIN proteins AS meso_proteins ON pairs.meso_pid = meso_proteins.pid
+                WHERE thermo_taxa.temperature>={T}
+                ORDER BY RANDOM()
+                LIMIT 100
+            """).df()
+            actual_protein_pairs[T] = data
+        con.close()
+        logger.info(f"Got actual meso and thermo proteins based on T")
 
-    # run esm and check outputs
-    random_meso_results = run_esm_on_df(random_meso_proteins)
-    random_intervals = {}
-    for T, random_thermo_proteins in random_thermo_proteins_dict.items():
-        # run thermo proteins in esm
-        random_thermo_results = run_esm_on_df(random_thermo_proteins)
-        meso_tensor = torch.vstack(tuple(random_meso_results['tensor'].values))
-        thermo_tensor = torch.vstack(tuple(random_thermo_results['tensor'].values))
-        assert len(meso_tensor) == len(thermo_tensor)
-        print(meso_tensor.shape)
+        # run esm and check outputs
+        random_meso_results = run_esm_on_df(random_meso_proteins)
+        random_distances = {}
+        for T, random_thermo_proteins in random_thermo_proteins_dict.items():
+            # run thermo proteins in esm
+            random_thermo_results = run_esm_on_df(random_thermo_proteins)
+            meso_tensor = torch.vstack(tuple(random_meso_results['tensor'].values))
+            thermo_tensor = torch.vstack(tuple(random_thermo_results['tensor'].values))
+            assert len(meso_tensor) == len(thermo_tensor)
+            print(meso_tensor.shape)
 
-        # compute distance between meso and thermo random proteins
-        distances = (meso_tensor - thermo_tensor).pow(2).sum(axis=1).sqrt().numpy()
-        logger.info(f"Mean, std of ESM distance for random pairs with thermohpile T>{T}: {distances.mean()}, {distances.std()}")
+            # compute distance between meso and thermo random proteins
+            distances = (meso_tensor - thermo_tensor).pow(2).sum(axis=1).sqrt()
+            random_distances[T] = distances
+            logger.info(f"Mean, std of ESM distance for random pairs with thermohpile T>{T}: {distances.mean()}, {distances.std()}")
+        torch.save(random_distances, './tmp/random_distances.pt')
 
-        # estimate CI on low distance
-        res = scipy.stats.bootstrap(
-            (distances,),
-            statistic = lambda x: np.quantile(x, 0.05),
-            confidence_level = 0.95,
-        )
-        interval = res.confidence_interval
-        logger.info(f"Confidence interval on 5th percentile ESM distance: {interval}")
-        random_intervals[T] = interval
+        # repeat the process, but use actual protein pairs
+        actual_distances = {}
+        for T, data in actual_protein_pairs.items():
+            # split the data into meso and thermo stuff
+            meso_sequences = data[['meso_pid', 'meso_seq']].rename(columns={'meso_pid': 'pid', 'meso_seq': 'protein_seq'})
+            thermo_sequences = data[['thermo_pid', 'thermo_seq']].rename(columns={'thermo_pid': 'pid', 'thermo_seq': 'protein_seq'})
 
-    # repeat the process, but use actual protein pairs
-    actual_intervals = {}
-    for T, (meso_sequences, thermo_sequences) in actual_protein_pairs.items():
-        # run thermo proteins in esm
-        meso_results = run_esm_on_df(meso_sequences)
-        thermo_results = run_esm_on_df(thermo_sequences)
-        meso_tensor = torch.vstack(tuple(meso_results['tensor'].values))
-        thermo_tensor = torch.vstack(tuple(thermo_results['tensor'].values))
-        assert len(meso_tensor) == len(thermo_tensor)
+            # run thermo proteins in esm
+            meso_results = run_esm_on_df(meso_sequences)
+            thermo_results = run_esm_on_df(thermo_sequences)
+            meso_tensor = torch.vstack(tuple(meso_results['tensor'].values))
+            thermo_tensor = torch.vstack(tuple(thermo_results['tensor'].values))
+            assert len(meso_tensor) == len(thermo_tensor)
 
-        # compute distance between meso and thermo random proteins
-        distances = (meso_tensor - thermo_tensor).pow(2).sum(axis=1).sqrt().numpy()
-        logger.info(f"Mean, std of ESM distance for actual pairs with thermohpile T>{T}: {distances.mean()}, {distances.std()}")
+            # compute distance between meso and thermo random proteins
+            distances = (meso_tensor - thermo_tensor).pow(2).sum(axis=1).sqrt()
+            logger.info(f"Mean, std of ESM distance for actual pairs with thermohpile T>{T}: {distances.mean()}, {distances.std()}")
+            actual_distances[T] = distances
+        torch.save(actual_distances, './tmp/actual_distances.pt')
 
-        # estimate CI on low distance
-        res = scipy.stats.bootstrap(
-            (distances,),
-            statistic = lambda x: np.quantile(x, 0.95),
-            confidence_level = 0.95,
-        )
-        interval = res.confidence_interval
-        logger.info(f"Confidence interval on 95th percentile ESM distance: {interval}")
-        actual_intervals[T] = interval
+    # make plot of all distributions
+    fig, ax = plt.subplots(1, 1, figsize=(5, 5))
+    for T, distances in random_distances.items():
+        break
+    sns.kdeplot(data=random_distances[T], ax=ax, label=f'Random pairs, T thermo>{T}')
+    sns.kdeplot(data=actual_distances[T], ax=ax, label=f'Actual pairs, T thermo>{T}')
+    sns.kdeplot(data=hait_distances, ax=ax, label=f'Hait et al. pairs')
+    ax.set_xlabel('ESM distance')
+    ax.set_ylabel('Observed probability')
+    ax.legend()
+    plt.savefig('./data/plots/esm_distance_hist.png', dpi=300, bbox_inches='tight')
+    
+    print(actual_distances.items)
 
-    # make plot of distance interval vs T
-    fig, ax = plt.subplots()
-    random_low = [i[0] for i in random_intervals.values()]
-    random_high = [i[1] for i in random_intervals.values()]
-    actual_low = [i[0] for i in actual_intervals.values()]
-    actual_high = [i[1] for i in actual_intervals.values()]
-    ax.fill_between(temps, random_low, random_high, alpha=0.5, label="5pctile random protein pair distance")
-    ax.fill_between(temps, actual_low, actual_high, alpha=0.5, label="95pctile actual protein pair distance")
-    ax.set_xlabel('Thermophilic temperature (C)')
-    ax.set_ylabel('ESM Distance')
-    ax.set_title('5th percentile ESM distance CI for meso <40C')
-    fig.savefig('./data/plots/esm_distance_ci.png', dpi=300, bbox_inches='tight'
-    )
+    # do some t tests
+    t_tests_to_hait = {}
+    for T, random_distance in random_distances.items():
+        for T2, actual_distance in actual_distances.items():
+            if np.isclose(T, T2):
+                break
+        _, p_actual = scipy.stats.ttest_ind(hait_distances, actual_distance, alternative='less')
+        _, p_random = scipy.stats.ttest_ind(hait_distances, random_distance, alternative='less')
+        t_tests_to_hait[float(T)] = {'our_pairs_p_value': float(p_actual), 'random_pairs_p_value': float(p_random)}
+    
+    # save metrics
+    with open('./data/metrics/s2.X_esm.yaml', 'w') as f:
+        yaml_dump(t_tests_to_hait, f)
 
 
 
